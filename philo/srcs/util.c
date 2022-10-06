0e715ef2bbf1aa6ac87b1caca658143d4e6d2b35
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2022-10-07 00:58:11 +0900

    update

--- philo/srcs/util.c
+++ philo/srcs/util.c
@@ -6,7 +6,7 @@
 /*   By: nfukuma <nfukuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 23:28:32 by nfukuma           #+#    #+#             */
-/*   Updated: 2022/10/07 00:49:40 by nfukuma          ###   ########.fr       */
+/*   Updated: 2022/10/07 00:53:59 by nfukuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +35,8 @@ int	util_atoi(char *str)
 
 void	util_put_log(t_each_philo *each, long now_us, char *color, char *msg)
 {
-	pthread_mutex_lock(&each->last_eat_mutex_t);
 	printf("%s%ld\t%d%s\e[m\n", color, (now_us - each->initial_us)
 		/ 1000, each->philo_id_num, msg);
-	pthread_mutex_unlock(&each->last_eat_mutex_t);
 	return ;
 }
 
