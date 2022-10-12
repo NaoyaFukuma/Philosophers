8d52d207e754db6a946e4f26dfcc27ae51deb81b
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2022-10-12 12:26:01 +0900

    update

--- philo/srcs/each_philo_routine.c
+++ philo/srcs/each_philo_routine.c
@@ -6,7 +6,7 @@
 /*   By: nfukuma <nfukuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 16:03:01 by nfukuma           #+#    #+#             */
-/*   Updated: 2022/10/12 12:25:05 by nfukuma          ###   ########.fr       */
+/*   Updated: 2022/10/12 12:25:58 by nfukuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,8 @@ static int	eat_philo(t_each_philo *each)
 	{
 		pthread_mutex_unlock(each->left_side_fork);
 		pthread_mutex_unlock(each->right_side_fork);
+	pthread_mutex_unlock(&(each->last_eat_mutex_t));
+	pthread_mutex_unlock(&each->philo_env->printf_mutex_t);
 		return (OTHER_PHILO_DEAD);
 	}
 	pthread_mutex_unlock(&(each->last_eat_mutex_t));
