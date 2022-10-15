bd0bef556a097d0fe1725a9db924b3f37134350e
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2022-10-16 00:57:53 +0900

    update

--- philo/srcs/each_philo_routine.c
+++ philo/srcs/each_philo_routine.c
@@ -6,13 +6,13 @@
 /*   By: nfukuma <nfukuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 16:03:01 by nfukuma           #+#    #+#             */
-/*   Updated: 2022/10/16 00:55:45 by nfukuma          ###   ########.fr       */
+/*   Updated: 2022/10/16 00:57:49 by nfukuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static int	take_fork_p(t_each_p *each);
+static int	take_right_fork_p(t_each_p *each);
 static int	eat_p(t_each_p *each);
 static int	sleep_p(t_each_p *each);
 static int	think_p(t_each_p *each);
